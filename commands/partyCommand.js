a105925b0ad22ffd6c98b8997929858168655b3f
Namu <49788407+namuuu@users.noreply.github.com>
2023-01-26 14:20:47 +0100

    ongoing: invite system

--- commands/partyCommand.js
+++ commands/partyCommand.js
@@ -10,7 +10,7 @@ module.exports = {
     authorId = message.author.id;
 
     if(args.length == 0) {
-        party.sendError(message);
+        party.sendError(message, "You need to specify a party command!");
         return;
     }   
 
@@ -18,14 +18,29 @@ module.exports = {
         case "help":
             party.sendHelp(message);
             break;
+        case "invite":
+            if(args.length == 1) {
+                party.sendError(message, "You need to mention someone to invite them!");
+                return;
+            }
+            if(!message.mentions.members.first()) {
+                party.sendError(message, "You need to mention someone to invite them!");
+                return;
+            }
+            party.invite(message, message.mentions.members.first().id);
         case "display":
-            if(args.length > 1)
-                party.displayParty(message, message.mentions.members.first().id);
-            else 
+            if(args.length > 1) {
+                const mention = message.mentions.members.first();
+                if(!mention) {
+                    party.sendError(message, "The person you mentionned is invalid");
+                } else
+                    party.displayParty(message, mention.id);
+                break;
+            } else 
                 party.displayParty(message, authorId);
             break;
         default:
-            party.sendError(message);
+            party.sendError(message, "This party command does not exist!");
             break;
     }
   }
