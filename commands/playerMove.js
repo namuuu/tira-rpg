66caf992d7e8f19b0754fb6c627d9f0dcdbab69f
Namu <49788407+namuuu@users.noreply.github.com>
2023-03-13 06:49:10 +0100

    feature: improved t.move embed

--- commands/playerMove.js
+++ commands/playerMove.js
@@ -1,4 +1,4 @@
-const { Client, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
+const { Client, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const player = require("../utils/playerUtils.js");
 const locationJSON = require('../data/location.json');
 const zonesData = require('../data/zones.json');
@@ -9,18 +9,17 @@ module.exports = {
     requireCharacter: true,
     async execute(message, args) {
         var availableLocations = []; // Array of locations that the player can go to
+        const playerInfo = await player.getData(message.author.id, "info");
+        const playerInventory = await player.getData(message.author.id, "inventory");
+        const playerZone = zonesData[playerInfo.location];
     
         for(const location of Object.values(locationJSON)) {
             for(const zone of location.zones) {
                 const zoneData = zonesData[zone]; // Data of the specific zone
-                const playerInfo = await player.getData(message.author.id, "info");
-                const playerInventory = await player.getData(message.author.id, "inventory");
 
                 if(playerInfo.location == zone) {
                     continue;
                 }
-
-                console.log(zoneData);
 
                 if(zoneData.required.level != null) {
                     if(playerInfo.level < zone.required.level) {
@@ -29,7 +28,7 @@ module.exports = {
                 }
 
                 if(zoneData.required.items != null) {
-                    for(const item of zone.required.items) {
+                    for(const item of zoneData.required.items) {
                         if(!playerInventory.items.includes(item)) {
                             continue;
                         }
@@ -37,22 +36,36 @@ module.exports = {
                 }
 
                 availableLocations.push({
-                    label: zone,
+                    label: zoneData.name,
                     value: zone,
-                    description: location.name
+                    description: zoneData.description
                 })
             }
         }
 
+        const embed = new EmbedBuilder()
+            .setDescription('You are currently in:')
+            .addFields(
+                { name: playerZone.name, value: 'Where would you want to go?'},
+            )
+            .setFooter({text: 'Need to learn more about where you are? Use the command: t.location'})
 
-        const row = new ActionRowBuilder()
+        const slider = new ActionRowBuilder()
             .addComponents(
                 new StringSelectMenuBuilder()
                     .setCustomId('locationChoice-' + message.author.id)
-                    .setPlaceholder('Nothing selected')
+                    .setPlaceholder('Select a place here!')
                         .addOptions(availableLocations),
             );
+
+        const button = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('location-far_travel-' + message.author.id)
+                    .setLabel('Go somewhere else')
+                    .setStyle(ButtonStyle.Secondary)
+            );
     
-        return message.reply({content: 'Choose the location where you want to go !', components: [row] });
+        message.reply({embeds: [embed], components: [slider, button] });
     }
 }
