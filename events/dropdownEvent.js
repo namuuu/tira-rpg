7284306b5d76a22bffd26b2f96b5da0933645ece
Namu <leevan.david@pit.fr>
2023-04-25 10:42:26 +0200

    feat: buttons, modals and dropdowns don't crash

--- events/dropdownEvent.js
+++ events/dropdownEvent.js
@@ -30,104 +30,108 @@ module.exports = {
         //console.log(command);
         //console.log(args);
 
-        switch(command) {
-            case 'displayInventory':
-                if(!(await player.doesExists(user.id))) return;
-                inv.displayInventory(authorId, interaction);
-                break;
-            case 'inventory_selector':
-                if(!(await player.doesExists(user.id)) || user.id != args[0]) return;
-                inv.display(user, interaction, interaction.values[0], true);
-                break;
-            case 'combat_ability_selector':
-                if(!(await player.doesExists(user.id))) return;
-                combat.receiveAbilitySelector(interaction);
-                break;
-            case 'combat_target_selector':
-                if(!(await player.doesExists(user.id))) return;
-                combat.receiveTargetSelector(interaction);
-                break;
-            case 'classChoice':
-                if(args[0] != interaction.user.id) {
-                    interaction.channel.send("If you would like to start your own adventure, please use the t.init commande yourself ! " + "<@" + interaction.user.id + ">");
-                    return;
-                }
-
-                await interaction.message.delete();
-
-                player.create(interaction.user.id, interaction.values[0]);
-
-                var playerClass = JSON.parse(fs.readFileSync('./data/classes.json'))
+        try {
+            switch(command) {
+                case 'displayInventory':
+                    if(!(await player.doesExists(user.id))) return;
+                    inv.displayInventory(authorId, interaction);
+                    break;
+                case 'inventory_selector':
+                    if(!(await player.doesExists(user.id)) || user.id != args[0]) return;
+                    inv.display(user, interaction, interaction.values[0], true);
+                    break;
+                case 'combat_ability_selector':
+                    if(!(await player.doesExists(user.id))) return;
+                    combat.receiveAbilitySelector(interaction);
+                    break;
+                case 'combat_target_selector':
+                    if(!(await player.doesExists(user.id))) return;
+                    combat.receiveTargetSelector(interaction);
+                    break;
+                case 'classChoice':
+                    if(args[0] != interaction.user.id) {
+                        interaction.channel.send("If you would like to start your own adventure, please use the t.init commande yourself ! " + "<@" + interaction.user.id + ">");
+                        return;
+                    }
+    
+                    await interaction.message.delete();
+    
+                    player.create(interaction.user.id, interaction.values[0]);
+    
+                    var playerClass = JSON.parse(fs.readFileSync('./data/classes.json'))
+                    
+                    const displayEmbed = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle(`:crossed_swords:  ${interaction.user.username}, you are a ${playerClass[interaction.values[0]].name} now !  :crossed_swords:`)
+                    .addFields( 
+                        { name: 'This is it... The beginning', value: "Are you ready for a whole new adventure ?" }
+                    )
+                    .setThumbnail(interaction.user.displayAvatarURL());
                 
-                const displayEmbed = new EmbedBuilder()
-                .setColor(0x0099FF)
-                .setTitle(`:crossed_swords:  ${interaction.user.username}, you are a ${playerClass[interaction.values[0]].name} now !  :crossed_swords:`)
-                .addFields( 
-                    { name: 'This is it... The beginning', value: "Are you ready for a whole new adventure ?" }
-                )
-                .setThumbnail(interaction.user.displayAvatarURL());
-            
-                await interaction.channel.send({embeds: [displayEmbed]});
-                break;
-            case 'shopChoice':
-                if(args[0] != interaction.user.id) {
-                    interaction.channel.send("If you would like to shop with your own character, please use the t.shop commande yourself ! " + "<@" + interaction.user.id + ">");
-                    return;
+                    await interaction.channel.send({embeds: [displayEmbed]});
+                    break;
+                case 'shopChoice':
+                    if(args[0] != interaction.user.id) {
+                        interaction.channel.send("If you would like to shop with your own character, please use the t.shop commande yourself ! " + "<@" + interaction.user.id + ">");
+                        return;
+                    }
+    
+                    selector.generateShopItemsSelector(interaction, interaction.values[0], "0", "0");
+    
+                    await interaction.message.delete();
+    
+                    break;
+                case 'shopItemChoice':
+                    if(args[0] != interaction.user.id) {
+                        interaction.channel.send("If you would like to shop with your own character, please use the t.shop commande yourself ! " + "<@" + interaction.user.id + ">");
+                        return;
+                    }
+    
+                    if (interaction.message.components[2] == undefined) {
+                        var quantity = interaction.message.components[1].components[0].customId.split('-')[4];
+                        var shop = interaction.message.components[1].components[0].customId.split('-')[2];
+                    } else {
+                        var quantity = interaction.message.components[2].components[0].customId.split('-')[4];
+                        var shop = interaction.message.components[2].components[0].customId.split('-')[2];
+                    }
+    
+                    await interaction.message.delete();
+    
+                    selector.generateShopItemsSelector(interaction, shop, interaction.values[0], quantity);
+                    break;
+                case 'shopAmountChoice':
+                    if(args[0] != interaction.user.id) {
+                        interaction.channel.send("If you would like to shop with your own character, please use the t.shop commande yourself ! " + "<@" + interaction.user.id + ">");
+                        return;
+                    }
+    
+                    if (interaction.message.components[2] == undefined) {
+                        var item = interaction.message.components[1].components[0].customId.split('-')[3];
+                        var shop = interaction.message.components[1].components[0].customId.split('-')[2];
+                    } else {
+                        var item = interaction.message.components[2].components[0].customId.split('-')[3];
+                        var shop = interaction.message.components[2].components[0].customId.split('-')[2];
+                    }
+    
+                    interaction.message.delete();
+    
+                    selector.generateShopItemsSelector(interaction, shop, item, interaction.values[0]);
+                    break;
+                default:
+                    break;
+    
+                
+            }   
+    
+            if(sliders.has(command)) {
+                try {
+                    await sliders.get(command).interact(interaction, interaction.values, args);
+                } catch (error) {
+                    console.error(error);
                 }
-
-                selector.generateShopItemsSelector(interaction, interaction.values[0], "0", "0");
-
-                await interaction.message.delete();
-
-                break;
-            case 'shopItemChoice':
-                if(args[0] != interaction.user.id) {
-                    interaction.channel.send("If you would like to shop with your own character, please use the t.shop commande yourself ! " + "<@" + interaction.user.id + ">");
-                    return;
-                }
-
-                if (interaction.message.components[2] == undefined) {
-                    var quantity = interaction.message.components[1].components[0].customId.split('-')[4];
-                    var shop = interaction.message.components[1].components[0].customId.split('-')[2];
-                } else {
-                    var quantity = interaction.message.components[2].components[0].customId.split('-')[4];
-                    var shop = interaction.message.components[2].components[0].customId.split('-')[2];
-                }
-
-                await interaction.message.delete();
-
-                selector.generateShopItemsSelector(interaction, shop, interaction.values[0], quantity);
-                break;
-            case 'shopAmountChoice':
-                if(args[0] != interaction.user.id) {
-                    interaction.channel.send("If you would like to shop with your own character, please use the t.shop commande yourself ! " + "<@" + interaction.user.id + ">");
-                    return;
-                }
-
-                if (interaction.message.components[2] == undefined) {
-                    var item = interaction.message.components[1].components[0].customId.split('-')[3];
-                    var shop = interaction.message.components[1].components[0].customId.split('-')[2];
-                } else {
-                    var item = interaction.message.components[2].components[0].customId.split('-')[3];
-                    var shop = interaction.message.components[2].components[0].customId.split('-')[2];
-                }
-
-                interaction.message.delete();
-
-                selector.generateShopItemsSelector(interaction, shop, item, interaction.values[0]);
-                break;
-            default:
-                break;
-
-            
-        }   
-
-        if(sliders.has(command)) {
-            try {
-                await sliders.get(command).interact(interaction, interaction.values, args);
-            } catch (error) {
-                console.error(error);
             }
+        } catch (error) {
+            console.error(error);
         }
     }
 }
